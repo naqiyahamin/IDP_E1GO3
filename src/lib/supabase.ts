bbf98acdb9c4043f3f4f15cb9625bd98185050ec
NAQIYAH BINTI AMINUDDIN <naqiyah@graduate.utm.my>
2026-06-21 16:07:43 +0800

    Updated supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -4,6 +4,11 @@ const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+  auth: {
+    persistSession: false,
+    autoRefreshToken: false,
+    detectSessionInUrl: true,
+  },
   realtime: {
     params: {
       eventsPerSecond: 10,
